1802acd7a7600ce636e001d8510610baadbdb831
MahdiHaeri <mahdihaerim@gmail.com>
2023-10-22 01:22:47 +0330

    Add User model, migraiton, admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'firstName', 'lastName', 'email', 'password', 'phoneNumber',
+        'country', 'birthday', 'created_at', 'updated_at')
+
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phoneNumber = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    birthday = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.firstName + " " + self.lastName
