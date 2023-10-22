d77851076de39f4e0a40493bb53e913d93fd400b
MahdiHaeri <mahdihaerim@gmail.com>
2023-10-22 14:12:50 +0330

    Add block model in users app

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import User, Follow
+from users.models import User, Follow, Block
 
 
 # Register your models here.
@@ -16,5 +16,11 @@ class FollowAdmin(admin.ModelAdmin):
         'id', 'follower_id', 'followed_id', 'created_at', 'updated_at')
 
 
+class BlockAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'blocker_id', 'blocked_id', 'created_at', 'updated_at')
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Follow, FollowAdmin)
+admin.site.register(Block, BlockAdmin)
--- users/models.py
+++ users/models.py
@@ -28,3 +28,14 @@ class Follow(models.Model):
 
     def __str__(self):
         return str(self.follower_id) + " follows " + str(self.followee_id)
+
+
+class Block(models.Model):
+    id = models.AutoField(primary_key=True)
+    blocker_id = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='blockers')
+    blocked_id = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='blocks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.blocker_id) + " blocks " + str(self.blocked_id)
