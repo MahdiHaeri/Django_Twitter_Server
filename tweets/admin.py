1594b20742ae5a8b16cf118b9c5ad3126f893731
MahdiHaeri <mahdihaerim@gmail.com>
2023-10-22 13:48:34 +0330

    Add tweet, retweet, reply, quote model

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from tweets.models import Tweet, Retweet, ReplyTweet, QuoteTweet
+
+
 # Register your models here.
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user_id', 'content', 'replyCount', 'retweetCount', 'likeCount',
+        'created_at', 'updated_at')
+
+
+class RetweetAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user_id', 'content', 'replyCount', 'retweetCount', 'likeCount',
+        'created_at', 'updated_at')
+
+
+class ReplyTweetAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user_id', 'content', 'replyCount', 'retweetCount', 'likeCount',
+        'created_at', 'updated_at')
+
+
+class QuoteTweetAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user_id', 'content', 'replyCount', 'retweetCount', 'likeCount',
+        'created_at', 'updated_at')
+
+
+admin.site.register(Tweet)
+admin.site.register(Retweet)
+admin.site.register(ReplyTweet)
+admin.site.register(QuoteTweet)
--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    content = models.CharField(max_length=50)
+    replyCount = models.IntegerField()
+    retweetCount = models.IntegerField()
+    likeCount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Retweet(Tweet):
+    retweet_id = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='retweets')
+
+    def __str__(self):
+        return self.content
+
+
+class ReplyTweet(Tweet):
+    reply_id = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='replies')
+
+    def __str__(self):
+        return self.content
+
+
+class QuoteTweet(Tweet):
+    quote_id = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name='quotes')
+
+    def __str__(self):
+        return self.content
