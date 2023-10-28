84c0bff7abaf8c4d7ea36bfa97d7fae3205ca8ad
MahdiHaeri <mahdihaerim@gmail.com>
2023-10-28 17:59:44 +0330

    Add follows and blocks to django rest framework

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from users.models import User
+from users.models import User, Follow, Block
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = '__all__'
+
+
+class BlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Block
         fields = '__all__'
