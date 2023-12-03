c4231636992d2b19583ac90770a00a936c9a9030
MahdiHaeri <mahdihaerim@gmail.com>
2023-12-03 03:41:57 +0330

    Add Simple JWT and config that in project + add /token, /token/refresh, /token/verify

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from tweets import views as tweets_views
 from users import views as users_views
 
@@ -6,4 +8,9 @@ urlpatterns = [
     path('', include("users.urls")),
     path('', include("tweets.urls")),
     path('profiles/', include("profiles.urls")),
+
+    # Simple JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
