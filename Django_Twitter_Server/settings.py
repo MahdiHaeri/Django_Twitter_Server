03696b08051ed637033cb3224f2e25c2e0c90e4d
MahdiHaeri <mahdihaerim@gmail.com>
2024-02-06 14:50:55 +0330

    Change postgre to sqlite3

--- Django_Twitter_Server/settings.py
+++ Django_Twitter_Server/settings.py
@@ -146,19 +146,19 @@ WSGI_APPLICATION = 'Django_Twitter_Server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'twitter',
-        'USER': 'postgres',
-        'PASSWORD': 'mahdi',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'twitter',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'mahdi',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 
 
